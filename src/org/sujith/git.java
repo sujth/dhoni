c1c8ec4081cc6ea9fb08aaba858142ca0a19bc11
vijayboss <vijayajith58@gmail.com>
2021-08-04 17:20:09 +0530

    started

--- src/org/sujith/git.java
+++ src/org/sujith/git.java
@@ -1,5 +1,12 @@
 package org.sujith;
 
 public class git {
+	private void dhoni() {
+		System.out.println("gun");
+}
+	public static void main(String[] args) {
+		git g= new git();
+		g.dhoni();
+		}
 
 }
