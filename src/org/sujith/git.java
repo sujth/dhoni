dca2dcb333ffe9381d298c82a27499afa03bf554
vijayboss <vijayajith58@gmail.com>
2021-08-04 17:25:24 +0530

    star

--- src/org/sujith/git.java
+++ src/org/sujith/git.java
@@ -1,5 +1,12 @@
 package org.sujith;
 
 public class git {
+	private void kholi() {
+		System.out.println("gun");
+}
+	public static void main(String[] args) {
+		git g= new git();
+		g.kholi();
+		}
 
 }
